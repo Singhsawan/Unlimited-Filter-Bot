77a78dc8c4c4918d130be2b2f018feb5051dd5c3
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-05 08:01:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @GreyMatter_Bots</b>
+<b>© @J_shree_ram</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
 
-⭕️<b>Creater :</b> @GreyMatter_Bots    
+⭕️<b>Creater :</b> @J_shree_ram    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
